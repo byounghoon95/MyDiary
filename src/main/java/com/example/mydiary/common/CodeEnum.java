57445634ddcae845348cb49448604aca9cf1c2cc
bhoon <toltolyibh@gmail.com>
2023-02-11 21:57:55 +0900

    RefreshToken 관련 코드값 추가

--- src/main/java/com/example/mydiary/common/CodeEnum.java
+++ src/main/java/com/example/mydiary/common/CodeEnum.java
@@ -10,7 +10,9 @@ public enum CodeEnum {
     SUCCESS(HttpStatus.OK,"SUCCESS"),
     DUPLICATED_MEMBER(HttpStatus.CONFLICT,"Member id is duplicated"),
     NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND,"Member id is not existed"),
+    NOT_FOUND_REFRESH_TOKEN(HttpStatus.NOT_FOUND,"RefreshToken is not existed"),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED,"Password is incorrect"),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED,"Token is expired"),
     UNKNOWN_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"UNKNOWN_ERROR");
 
     private HttpStatus code;
