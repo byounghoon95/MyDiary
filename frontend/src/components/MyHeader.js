0078eef9b8e2ab89edccbe23600295a8903c6a51
bhoon <toltolyibh@gmail.com>
2023-02-12 10:27:02 +0900

    헤더 스타일 변경

--- frontend/src/components/MyHeader.js
+++ frontend/src/components/MyHeader.js
@@ -2,6 +2,7 @@ import React from "react";
 const MyHeader = ({ leftChild, rightChild, headText }) => {
     return (
         <header>
+            <div className="head_top">이병훈님의 일기</div>
             <div className="head_btn_left">{leftChild}</div>
             <div className="head_text">{headText}</div>
             <div className="head_btn_right">{rightChild}</div>
