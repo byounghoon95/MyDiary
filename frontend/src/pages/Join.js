02763731bd33a57be470b67d100d58bdad13cc1b
bhoon <toltolyibh@gmail.com>
2023-02-12 10:25:32 +0900

    프론트 jwt 로직 추가

--- frontend/src/pages/Join.js
+++ frontend/src/pages/Join.js
@@ -1,20 +1,22 @@
 import React, {useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Join = () => {
   const navigate = useNavigate();
 
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
   const [idValid, setIdValid] = useState(true);
   const [pwValid, setPwValid] = useState(false);
+
   const inputStyle = {
     width: 'calc(100% - 60px)',
     marginBottom:0
   }
 
   const handleId = (e) => {
-    console.log(e.target.value);
     setId(e.target.value);
   };
 
@@ -22,12 +24,15 @@ const Join = () => {
     setPassword(e.target.value);
   };
 
-  const login = () => {
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
+  const moveLogin = () => {
     navigate("/");
   };
 
   const doubleCheck = () => {
-    console.log("asdf");
     if (id.length > 5) {
       setIdValid(true);
     } else {
@@ -37,7 +42,13 @@ const Join = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    navigate("/home");
+    axios.post("/api/users/join", {
+      memId: id,
+      password: password,
+      name: name,
+    })
+        .then(response => console.log(response));
+    navigate("/");
   };
 
   return (
@@ -68,10 +79,11 @@ const Join = () => {
             type="text"
             name="name"
             placeholder="name"
+            onChange={handleName}
         />
         <input type="submit" value="회원가입" />
         <div className="login-box">
-          <span onClick={login}>로그인</span><span> | </span><span>아이디 찾기</span><span> | </span><span>비밀번호 찾기</span>
+          <span onClick={moveLogin}>로그인</span><span> | </span><span>아이디 찾기</span><span> | </span><span>비밀번호 찾기</span>
         </div>
       </form>
     </div>
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -8,28 +8,50 @@ const Login = () => {
     id: "",
     password: "",
   });
-
   // e.target.name = input의 name(id,password)
   const onChangeAccount = (e) => {
-    setAccount({
-      ...account,
-      [e.target.name]: e.target.value,
+    setAccount((prev) => {
+      return {...prev, [e.target.name]: e.target.value};
     });
+  };
+
+  const param = {
+    memId: account.id,
+    password: account.password,
   };
 
   const join = () => {
     navigate("/join");
   };
 
+  const onLoginSuccess = (response) => {
+    const { accessToken } = response.data.data.accessToken;
+    const expireTime = response.data.data.accessTokenExpireTime
+    // accessToken 설정
+    axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+
+    // accessToken 만료하기 1분 전에 로그인 연장
+    setTimeout(onMakeRefreshToken, expireTime - 60000);
+    navigate("/home");
+  }
+
+  const onLoginError = () => {
+    alert("아이디 혹은 패스워드가 일치하지 않습니다");
+    setAccount((prev) => {
+      return {...prev, id: "", password: ""};
+    });
+  }
+
+  const onMakeRefreshToken = () => {
+    axios.post('/api/users/reissue', param)
+        .then(onLoginSuccess);
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post("/api/users/login",
-        {
-          memId: account.id,
-          password: account.password,
-        })
-        .then(response=> console.log(response.data))
-    navigate("/home");
+    axios.post("/api/users/login", param)
+        .then(onLoginSuccess)
+        .catch(onLoginError);
   };
 
   return (
@@ -40,12 +62,14 @@ const Login = () => {
           type="text"
           name="id"
           onChange={onChangeAccount}
+          value={account.id}
           placeholder="id"
         />
         <input
           type="password"
           name="password"
           onChange={onChangeAccount}
+          value={account.password}
           placeholder="password"
         />
         {/*<label htmlFor="remember-check">*/}
