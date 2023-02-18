ef494ec6274fe5673794540dc61e804019d5388a
bhoon <toltolyibh@gmail.com>
2023-02-18 14:27:14 +0900

    CodeEnum 에러 추가

--- src/main/java/com/example/mydiary/common/CodeEnum.java
+++ src/main/java/com/example/mydiary/common/CodeEnum.java
@@ -11,8 +11,11 @@ public enum CodeEnum {
     DUPLICATED_MEMBER(HttpStatus.CONFLICT,"Member id is duplicated"),
     NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND,"Member id is not existed"),
     NOT_FOUND_REFRESH_TOKEN(HttpStatus.NOT_FOUND,"RefreshToken is not existed"),
+    NOT_FOUND_AUTHORITY(HttpStatus.UNAUTHORIZED, "No Authority"),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED,"Password is incorrect"),
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED,"Token is expired"),
+    LOGOUT_MEMBER(HttpStatus.NOT_FOUND,"Member is logout"),
+    BAD_TOKEN(HttpStatus.UNAUTHORIZED,"Bad Token"),
     UNKNOWN_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"UNKNOWN_ERROR");
 
     private HttpStatus code;
