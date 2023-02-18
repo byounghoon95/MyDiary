cb69062cc7569676fe9aae22b88b7e38efbbaa2c
bhoon <toltolyibh@gmail.com>
2023-02-18 14:31:17 +0900

    MemberService 수정

--- src/main/java/com/example/mydiary/member/service/MemberService.java
+++ src/main/java/com/example/mydiary/member/service/MemberService.java
@@ -1,81 +1,132 @@
 package com.example.mydiary.member.service;
 
-import antlr.Token;
 import com.example.mydiary.common.CodeEnum;
-import com.example.mydiary.dto.MemberJoinDto;
-import com.example.mydiary.dto.MemberLoginDto;
-import com.example.mydiary.dto.TokenDto;
-import com.example.mydiary.dto.TokenRequestDto;
+import com.example.mydiary.dto.*;
+import com.example.mydiary.entity.Authority;
+import com.example.mydiary.entity.MemberAuth;
 import com.example.mydiary.entity.MemberEntity;
 import com.example.mydiary.entity.RefreshToken;
 import com.example.mydiary.exception.CustomException;
+import com.example.mydiary.jwt.CustomMemIdPasswordAuthToken;
+import com.example.mydiary.member.repository.AuthorityRepository;
 import com.example.mydiary.member.repository.MemberRepository;
 import com.example.mydiary.member.repository.RefreshTokenRepository;
-import com.example.mydiary.utils.JwtTokenUtil;
+import com.example.mydiary.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.HashSet;
+import java.util.Set;
+
+@Slf4j
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Service
 public class MemberService {
     private final MemberRepository memberRepository;
     private final RefreshTokenRepository refreshTokenRepository;
-    private final JwtTokenUtil jwtTokenUtil;
-
-    @Value("${jwt.token.secret}")
-    private String key;
-    @Value("${jwt.token.expired-ms}")
-    private Long expireTimeMs;
-    @Value("${jwt.token.refresh-ms}")
-    private Long refreshTimeMs;
+    private final AuthorityRepository authorityRepository;
+    private final TokenProvider tokenProvider;
+    private final PasswordEncoder encoder;
+    private final AuthenticationManager authenticationManager;
+    private final CustomUserDetailsService customUserDetailsService;
     @Transactional
-    public MemberJoinDto join(MemberJoinDto dto) {
+    public MemberJoinResponseDto join(MemberJoinRequestDto requestDto) {
         //memId 중복 체크
-        memberRepository.findByMemId(dto.getMemId())
+        memberRepository.findByMemId(requestDto.getMemId())
                 .ifPresent(member -> {
-                    throw new CustomException(CodeEnum.DUPLICATED_MEMBER, dto.getMemId() + " 는 이미 등록된 아이디입니다.");
+                    throw new CustomException(CodeEnum.DUPLICATED_MEMBER, requestDto.getMemId() + " 는 이미 등록된 아이디입니다.");
                 });
-        memberRepository.save(dto.toEntity());
-        return dto;
+        // DB 에서 ROLE_USER를 찾아서 권한으로 추가
+        Authority authority = authorityRepository
+                .findByAuthorityName(MemberAuth.ROLE_USER).orElseThrow(()->new CustomException(CodeEnum.NOT_FOUND_AUTHORITY,"권한이 없습니다"));
+        Set<Authority> set = new HashSet<>();
+        set.add(authority);
+
+        requestDto.setPassword(encoder.encode(requestDto.getPassword()));
+        memberRepository.save(requestDto.toEntity());
+
+        return new MemberJoinResponseDto(requestDto.getMemId(),requestDto.getName());
     }
     public TokenDto login(MemberLoginDto dto) {
-        MemberEntity member = memberRepository.findByMemId(dto.getMemId())
-                .orElseThrow(() -> new CustomException(CodeEnum.NOT_FOUND_MEMBER, dto.getMemId() + " 와 일치하는 아이디가 존재하지 않습니다."));
-        if (!member.getPassword().equals(dto.getPassword())) {
-            throw new CustomException(CodeEnum.INVALID_PASSWORD, "패스워드를 잘못 입력했습니다");
-        }
+        CustomMemIdPasswordAuthToken customMemIdPasswordAuthToken  = new CustomMemIdPasswordAuthToken(dto.getMemId(),dto.getPassword());
+        Authentication authenticate = authenticationManager.authenticate(customMemIdPasswordAuthToken);
 
-        TokenDto token = jwtTokenUtil.createToken(member.getMemId(), key, expireTimeMs, refreshTimeMs);
+        String memId = authenticate.getName();
+        MemberEntity member = customUserDetailsService.getMember(memId);
 
-        return token;
+        String accessToken = tokenProvider.createAccessToken(memId, member.getAuthorities());
+        String refreshToken = tokenProvider.createRefreshToken(memId, member.getAuthorities());
+
+        //refresh Token 저장
+        refreshTokenRepository.save(
+                RefreshToken.builder()
+                        .key(memId)
+                        .value(refreshToken)
+                        .build()
+        );
+
+        return tokenProvider.createTokenDto(accessToken,refreshToken);
     }
 
-    public TokenDto reissue(@RequestBody TokenRequestDto tokenRequestDto, @RequestBody MemberLoginDto memberLoginDto) {
-        //리프레시 토큰 만료 에러
-        if (!jwtTokenUtil.validationToken(tokenRequestDto.getRefreshToken(),key, memberLoginDto.getMemId())) {
-            throw new CustomException(CodeEnum.EXPIRED_TOKEN, "토큰이 만료되었습니다");
+    @Transactional
+    public TokenDto reissue(TokenRequestDto tokenRequestDto) {
+        /*
+         *  accessToken 은 JWT Filter 에서 검증되고 옴
+         * */
+        String originAccessToken = tokenRequestDto.getAccessToken();
+        String originRefreshToken = tokenRequestDto.getRefreshToken();
+
+        // refreshToken 검증
+        boolean refreshTokenFlag = tokenProvider.validateToken(originRefreshToken);
+
+        log.debug("refreshTokenFlag = {}", refreshTokenFlag);
+
+        //refreshToken 검증하고 상황에 맞는 오류를 내보낸다.
+        if (!refreshTokenFlag) {
+            throw new CustomException(CodeEnum.EXPIRED_TOKEN,"토큰의 유효기간이 끝났습니다");
         }
 
-        String accessToken = tokenRequestDto.getAccessToken();
-        MemberEntity member = memberRepository.findByMemId(memberLoginDto.getMemId())
-                .orElseThrow(() -> new CustomException(CodeEnum.NOT_FOUND_MEMBER, memberLoginDto.getMemId() + " 와 일치하는 아이디가 존재하지 않습니다."));
-        RefreshToken refreshToken = refreshTokenRepository.findByKey(member.getId())
-                .orElseThrow(() -> new CustomException(CodeEnum.NOT_FOUND_REFRESH_TOKEN, "리프레시 토큰값을 조회할 수 없습니다."));
+        // 2. Access Token 에서 Member Email 가져오기
+        Authentication authentication = tokenProvider.getAuthentication(originAccessToken);
 
-        //리프레시토큰 불일치 에러
-        if (!refreshToken.getToken().equals(tokenRequestDto.getRefreshToken())) {
-            throw new CustomException(CodeEnum.NOT_FOUND_REFRESH_TOKEN, "리프레시 토큰값을 조회할 수 없습니다.");
+        log.debug("Authentication = {}",authentication);
+        log.debug("Authentication.getName = {}",authentication.getName());
+
+
+        // 3. 저장소에서 memId 를 기반으로 Refresh Token 값 가져옴
+        RefreshToken refreshToken = refreshTokenRepository.findByKey(authentication.getName())
+                .orElseThrow(() -> new CustomException(CodeEnum.LOGOUT_MEMBER,"사용자가 로그아웃하였습니다.")); // 로그 아웃된 사용자
+
+
+        // 4. Refresh Token 일치하는지 검사
+        if (!refreshToken.getValue().equals(originRefreshToken)) {
+            throw new CustomException(CodeEnum.BAD_TOKEN,"토큰이 일치하지 않습니다.");
         }
 
-        TokenDto newToken = jwtTokenUtil.createToken(member.getMemId(), key, expireTimeMs, refreshTimeMs);
-        RefreshToken updatedRefreshToken = refreshToken.updateToken(newToken.getRefreshToken());
-        refreshTokenRepository.save(updatedRefreshToken);
+        // 5. 새로운 토큰 생성
+        String memId = tokenProvider.getMemIdByToken(originAccessToken);
+        MemberEntity member = customUserDetailsService.getMember(memId);
 
-        return newToken;
+        String newAccessToken = tokenProvider.createAccessToken(memId, member.getAuthorities());
+        String newRefreshToken = tokenProvider.createRefreshToken(memId, member.getAuthorities());
+        TokenDto tokenDto = tokenProvider.createTokenDto(newAccessToken, newRefreshToken);
+
+        log.debug("refresh Origin = {}",originRefreshToken);
+        log.debug("refresh New = {} ",newRefreshToken);
+
+        // 6. 저장소 정보 업데이트
+        refreshToken.updateValue(newRefreshToken);
+
+        // 토큰 발급
+        return tokenDto;
     }
 
 }
